dba5a571a32c2200413be64b6ba6fafc3d371548
shreefaw <shreefaw@gmail.com>
2021-04-22 21:59:47 +0300

    second commit

--- javascript.js
+++ javascript.js
@@ -1,13 +1,12 @@
 let number = prompt("Enter a number between 100 and 999",)||0;
-let number1,number2,number3=0;
-
- 
+let number1,number2,number3;
 if (number >= 100 && number <= 999) {
-   number1= parseInt(number.substr(0,1));
-   number2= parseInt(number.substr(1,1));
-   number3= parseInt(number.substr(2,1));
-    //Narcissistic= number1.pow(3)+ number2.pow(3)+ number3.pow(3);
-    Narcissistic= number1**3 + number2**3+number3**3;
+    number1= parseInt(number.substr(0,1));
+    number2= parseInt(number.substr(1,1));
+    number3= parseInt(number.substr(2,1));
+   
+    //Narcissistic= number1**3 + number2**3 + number3**3;
+    Narcissistic= Math.pow(number1,3) + Math.pow(number2,3) + Math.pow(number3,3);
      if (number == Narcissistic){
          alert("Narcissistic Number");
      }
@@ -18,5 +17,4 @@ if (number >= 100 && number <= 999) {
 else {
     alert("The number must be between 100 and 999");
 }
-
- 
+  
