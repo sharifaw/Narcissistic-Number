ce6699a9d1562d49e9658c73c03565324aa4e07b
shreefaw <shreefaw@gmail.com>
2021-04-25 09:58:05 +0300

    fix issue

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
-let number = +(prompt("Enter a number between 100 and 999",))||0;
-
-let number1= number.toString(0,1);
-let number2= number.toString(1,1);
-let number3= number.toString(2,1);
-
+let number = prompt("Enter a number between 100 and 999",)||0;
+let number1,number2,number3=0;
 if (number >= 100 && number <= 999) {
- 
+    number1= number.substr(0,1);
+    number2= number.substr(1,1);
+    number3= number.substr(2,1);
+
     //Narcissistic= number1**3 + number2**3 + number3**3;
     Narcissistic= Math.pow(number1,3) + Math.pow(number2,3) + Math.pow(number3,3);
      if (number == Narcissistic){
