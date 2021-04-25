3c2a12e544a97df7629cca4fe3863596f647014e
shreefaw <shreefaw@gmail.com>
2021-04-25 08:19:20 +0300

    editing if statements

--- javascript.js
+++ javascript.js
@@ -1,20 +1,21 @@
-let number = prompt("Enter a number between 100 and 999",)||0;
-let number1,number2,number3=0;
+let number = +(prompt("Enter a number between 100 and 999",))||0;
+
+let number1= number.toString(0,1);
+let number2= number.toString(1,1);
+let number3= number.toString(2,1);
+
 if (number >= 100 && number <= 999) {
-    number1= parseInt(number.substr(0,1));
-    number2= parseInt(number.substr(1,1));
-    number3= parseInt(number.substr(2,1));
-   
+ 
     //Narcissistic= number1**3 + number2**3 + number3**3;
     Narcissistic= Math.pow(number1,3) + Math.pow(number2,3) + Math.pow(number3,3);
      if (number == Narcissistic){
          alert("Narcissistic Number");
      }
+     
      else{
          alert("Not Narcissistic");
      }
- }
+}
 else {
     alert("The number must be between 100 and 999");
 }
-  
